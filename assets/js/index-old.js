6485a317e826b28da6598878173cec82263da11e
Ali Karbassi <ali@karbassi.com>
2021-11-12 09:18:35 -0600

    week 10

---
+++ assets/js/index-old.js
@@ -0,0 +1,45 @@
+// // variable that is a string
+// let name = 'Ali Karbassi';
+
+// console.log(name);
+
+// // variable named 'age' with your age value
+// let age = 24;
+
+// // output it to the console.
+// console.log(age);
+
+// // I want to know the price of a shoe
+// let shoes_price = '9.99';
+// console.log(shoes_price);
+// // console.log(typeof shoes_price);
+
+// // discount
+// let discount = 0.2;
+
+// // Get shoe price with discount
+// let sale = shoes_price * (1 - discount);
+
+// console.log(sale);
+
+// ----
+
+// string
+let a = ''
+
+// number
+let b = 1
+let c = 1.111
+let d = 9.81e4
+
+// arrays
+let toys = ['bert', 'erney', 'elmo']
+
+// console.log(toys);
+
+// objects
+let shoe = {
+  name: 'Kät',
+  price: 900,
+  stock: 12,
+}
